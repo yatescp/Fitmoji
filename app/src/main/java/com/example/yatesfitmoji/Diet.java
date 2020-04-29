e0e81cee2e9ead403b212d6f08620283307716a0
Thomas Marshall <tbmarshalljr@gmail.com>
2020-04-28 20:31:28 -0400

    Added Diet table

---
+++ app/src/main/java/com/example/yatesfitmoji/Diet.java
@@ -0,0 +1,60 @@
+package com.example.yatesfitmoji;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "Diets")
+
+public class Diet {
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "name")
+    private String name;
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "date")
+    private String date;
+
+    @NonNull
+    @ColumnInfo(name = "carbs")
+    private int carbs;
+
+    @NonNull
+    @ColumnInfo(name = "protein")
+    private int protein;
+
+    @NonNull
+    @ColumnInfo(name = "fat")
+    private int fat;
+
+    public Diet(@NonNull String name, @NonNull String date, @NonNull int carbs, @NonNull int protein, @NonNull int fat){
+        this.name = name;
+        this.date = date;
+        this.carbs = carbs;
+        this.protein = protein;
+        this.fat = fat;
+    }
+
+    @NonNull public String getName(){return name;}
+
+    @NonNull public String getDate(){return date;}
+
+    @NonNull public int getCarbs(){return carbs;}
+
+    @NonNull public int getProtein(){return protein;}
+
+    @NonNull public int getFat(){return fat;}
+
+    public void setName(@NonNull String name){this.name = name;}
+
+    public void setCarbs(@NonNull int carbs){this.carbs = carbs;}
+
+    public void setProtein(@NonNull int protein){this.protein = protein;}
+
+    public void setFat(@NonNull int fat){this.fat = fat;}
+
+    public void setDate(@NonNull String date){this.date = date;}
+}
---
+++ app/src/main/java/com/example/yatesfitmoji/DietDao.java
@@ -0,0 +1,37 @@
+package com.example.yatesfitmoji;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+@Dao
+public interface DietDao {
+
+    @Query("SELECT * FROM Diets")
+    LiveData<List<Diet>> getAllDiets();
+
+    @Query("SELECT * FROM Diets WHERE name = :nameKey LIMIT 1")
+    LiveData<Diet> getDiet(String nameKey);
+
+    //@Query("SELECT carbs FROM Workouts WHERE name = :nameKey LIMIT 1")
+//    LiveData<Diet> getCarbs(String nameKey);
+//
+//    @Query("SELECT protein FROM Workouts WHERE name = :nameKey LIMIT 1")
+//    LiveData<Diet> getPrptein(String nameKey);
+//
+//    @Query("SELECT fat FROM Workouts WHERE name = :nameKey LIMIT 1")
+//    LiveData<Diet> getFat(String nameKey);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Diet diet);
+
+    @Query("DELETE FROM Diets")
+    void deleteDiet();
+
+    @Delete
+    void delete(Diet diet);
+}
